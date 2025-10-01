36f8fd9837810d347375cd656761867043e17a1d
Logan Valeski <loganvaleski@gmail.com>
2025-10-01 14:18:43 -0400

    Transitioning naming convention to 'unboundapp' across all of codebase

--- android/app/src/main/java/com/unboundapp/MainApplication.kt
+++ android/app/src/main/java/com/unboundapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mvpfresh
+package com.unboundapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/unboundapp/AppDelegate.swift
+++ ios/unboundapp/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "mvp_fresh",
+      withModuleName: "unboundapp",
       in: window,
       launchOptions: launchOptions
     )
